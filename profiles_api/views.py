0014dd867723b976be0c0997e90dffc742636e0f
Prakash1099 <prakashukkaravel@gmail.com>
2026-09-11 02:07:03 +0530

    Added Hello API veiw

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+# APIView -> parent class from django_rest
+# We have to define all the url with HTTP methos
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+
+        # The format=None is to define a format suffix at the end of the end point url
+
+        an_apiview = [
+            'uses HTTP methods as funciton (get, post, patch, put, delete)',
+            'is similar to a traditional Django View',
+            'Gives you the most control ovet=r the application logic',
+            'Is mapped manually to URLs',
+        ]
+
+        #respnose should contain either Dict or List, the frontend will expect a json format response so either dict or list
+        return Response({
+            'message': 'Hello!',
+            'an_apiview': an_apiview,
+        })
+
+        
+
+
+
+    
